3b519c00327e4cdc9e8b09f22c410dec4b9dad92
magnurh <magnus.hestvik@gmail.com>
2017-08-22 22:30:23 +0200

    Changed variable names for user settings

--- src/main/java/no/uio/ifi/CaSPL/RunnableDockerApp.java
+++ src/main/java/no/uio/ifi/CaSPL/RunnableDockerApp.java
@@ -8,43 +8,50 @@ public class RunnableDockerApp {
 		
 		String dataSetName = "_CaSPL";
 		
-		int sizeOfDataSet = 10;
+		int sizeDataSet = 10;
 		
 		// CFM Parameters
 		int numberOfFeatures = 50;
-		int percentageOfCrossTreeConstraints = 35;
-		int maxPercentageOfVFs = 17;
-		int contextMaxSize = 13;
+		int percentageCTC = 35;
 		
-		int mandatoryProbability = 25;
-		int optionalProbability = 25;
-		int alternativeProbability = 25;
-		int orProbability = 25;
+		int probMand = 25;
+		int probOpt = 25;
+		int probAlt = 25;
+		int probOr = 25;
 		
-		int attributeRangeFrom = 0;
-		int attributeRangeTo = 100;
+		int maxBranchingFactor = 10;			//TODO: implement
+		int maxSetChildren = 5;					//TODO: implement
+		
+		int maxPercentageVFs = 20;
+		int minAttrValue = 0;
+		int maxAttrValue = 100;
+		int contextMaxSize = 10;
 		int contextMaxValue = 10;
 		
-		int maxTriesValidModel = 1;
-		int requiredNumberOfPathsFromRoot = 6;
-		int pathSearchDepth = 5;
-		int maxTriesPathRequirement = 20;
+		int maxTriesValidModel = 10;
+		int requiredNumberOfPathsFromRoot = 5;		// Advanced
+		int pathSearchDepth = 5;					// Advanced
+		int maxTriesPathRequirement = 0;			// Advanced
 		
-		//boolean hyvarrecInputScript = false;		// Implement as a setting to toggle returning hvr-script
+		boolean simpleMode = false;
+		boolean hyvarrecInputScript = false;		// Advanced
+		int hyvarrecPort = 4000;					// Advanced
 		
-		System.out.println("Hello World!");
-		System.out.println(System.getProperty("java.class.path"));
 		
-		DatasetGenerator generator = new DatasetGenerator(dataSetName, sizeOfDataSet, numberOfFeatures, percentageOfCrossTreeConstraints, maxPercentageOfVFs);
-		generator.setRelationshipParameters(mandatoryProbability, optionalProbability, alternativeProbability, orProbability);
-		generator.setMaxAttributeRange(attributeRangeFrom, attributeRangeTo);
+		DatasetGenerator generator = new DatasetGenerator(dataSetName, sizeDataSet, numberOfFeatures, percentageCTC, maxPercentageVFs);
+		generator.setRelationshipParameters(probMand, probOpt, probAlt, probOr);
+		generator.setMaxAttributeRange(minAttrValue, maxAttrValue);
 		generator.setRelativeContextSizeAndRange(contextMaxSize, contextMaxValue);
 		generator.setMaxTriesValidModelReasoner(maxTriesValidModel);
 		generator.setPathRequirements(requiredNumberOfPathsFromRoot, pathSearchDepth, maxTriesPathRequirement);
+		generator.setHyVarRecScriptSettings(hyvarrecInputScript, hyvarrecPort);
 		
 		try {
-			generator.generateCFMDataSet();
-			//generator.generateCFMDataSetWithoutRestrictions();
+			if(simpleMode){
+				generator.generateCFMDataSetWithoutRestrictions();
+			}else{
+				generator.generateCFMDataSet();
+			}
 		} catch (BettyException b) {
 			System.err.println(b.getMessage());
 		} catch (Exception e) {
