e43a0c38f8d38cddf024c2b0d7099195bb5900a4
magnurh <magnus.hestvik@gmail.com>
2017-08-23 22:41:01 +0200

    Changed name of main class

--- src/main/java/no/uio/ifi/CaSPL/CaSPLgen.java
+++ src/main/java/no/uio/ifi/CaSPL/CaSPLgen.java
@@ -12,7 +12,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class RunnableDockerApp {
+public class CaSPLgen {
 	
 	static String dataSetName = "CaSPL";
 	static Integer sizeDataSet = 10;
